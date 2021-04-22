4aff92f1b597f9219eae6ae8db4ea30310caf63e
estherzulema <estherzulema10@gmail.com>
2021-04-22 16:47:39 -0400

    widgets

---
+++ meu_primeiro_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      new MaterialApp(
+        title: 'Cores',
+        home: new Container(
+          color: Colors.white,
+          child: new Column(
+            children: [
+              Text('Ola, dev'),
+              Text('Tudo bem contigo ?'),
+            ],
+          ),
+        ),
+      ),
+    );
