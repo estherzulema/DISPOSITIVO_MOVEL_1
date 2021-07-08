fbeee34568f4b6399e0c03dcbe300f1d5175b975
estherzulema <estherzulema10@gmail.com>
2021-07-08 16:39:04 -0400

    VIDENTE_APP

--- vidente_app/lib/widgets/home.dart
+++ vidente_app/lib/widgets/home.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vidente_app/models/previsao_hora.dart';
-import 'package:vidente_app/services/previsao_services.dart';
+import 'package:vidente_app/services/previsao_service.dart';
 import 'package:vidente_app/widgets/proximas_temperaturas.dart';
 import 'package:vidente_app/widgets/resumo.dart';
 
@@ -11,14 +10,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<PrevisaoHora> ultimasPrevisoes;
+  Future<List<PrevisaoHora>> ultimasPrevisoes;
 
   @override
   void initState() {
     super.initState();
-    PrevisaoServices service = PrevisaoServices();
-    //pparei aqui//
+    carregarPrevisoes();
+  }
+
+  void carregarPrevisoes() {
+    PrevisaoService service = PrevisaoService();
     ultimasPrevisoes = service.recuperarUltimasPrevisoes();
+  }
+
+  Future<Null> atualizarPrevisoes() async {
+    setState(() => carregarPrevisoes());
   }
 
   @override
@@ -28,20 +34,53 @@ class _HomeState extends State<Home> {
         title: Text('Vidente'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Resumo(
-              cidade: 'Aquidauana',
-              temperaturaAtual: 33,
-              temperaturaMaxima: 39,
-              temperaturaMinima: 27,
-              descricao: 'Ensolarado',
-              numeroIcone: 1,
-            ),
-            Padding(padding: EdgeInsets.all(10)),
-            ProximasTemperaturas(previsoes: ultimasPrevisoes),
-          ],
+      body: RefreshIndicator(
+        onRefresh: atualizarPrevisoes,
+        child: Center(
+          child: FutureBuilder<List<PrevisaoHora>>(
+            future: ultimasPrevisoes,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<PrevisaoHora> previsoes = snapshot.data;
+                double temperaturaAtual = previsoes[0].temperatura;
+                double menorTemperatura = double.maxFinite;
+                double maiorTemperatura = double.negativeInfinity;
+                previsoes.forEach((obj) {
+                  if (obj.temperatura < menorTemperatura) {
+                    menorTemperatura = obj.temperatura;
+                  }
+
+                  if (obj.temperatura > maiorTemperatura) {
+                    maiorTemperatura = obj.temperatura;
+                  }
+                });
+
+                String descricao = previsoes[0].descricao;
+                int numeroIcone = previsoes[0].numeroIcone;
+
+                return Column(
+                  children: [
+                    Resumo(
+                      cidade: 'Aquidauana-MS',
+                      temperaturaAtual: temperaturaAtual,
+                      temperaturaMaxima: maiorTemperatura,
+                      temperaturaMinima: menorTemperatura,
+                      descricao: descricao,
+                      numeroIcone: numeroIcone,
+                    ),
+                    Padding(padding: EdgeInsets.all(10)),
+                    ProximasTemperaturas(
+                      previsoes: previsoes.sublist(1, previsoes.length),
+                    )
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Text('Erro ao carregar as previsões');
+              }
+
+              return CircularProgressIndicator();
+            },
+          ),
         ),
       ),
     );
